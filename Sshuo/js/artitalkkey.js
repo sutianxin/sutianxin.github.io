52efefa322785fc880925b1d17d5f6e1ed7ce251
sutianxin <763882220@qq.com>
2021-02-08 21:33:39 +0800

    Site updated: 2021-02-08 21:33:32

---
+++ Sshuo/js/artitalkkey.js
@@ -0,0 +1,15 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>new Artitalk({
+    appId: '0MahnTx5Kd1CDiuaMHvWRL6U-MdYXbMMI',
+    appKey: '2WVsr42NePn31UWlj2uNLviA',
+	pageSize: 1,
+	color1: "#b30070",
+	color2: "rgb(229, 91, 178)",
+});
+$(".power").css("padding","10px");
+$(".power").css("text-align","left");
+$("#switchUser").css("padding-right","10px");
+$("#pubShuo").css("padding-right","10px");
+$("#pubComment").css("padding-right","10px");
+$("#uploadSource").css("padding-right","10px");
+
+
