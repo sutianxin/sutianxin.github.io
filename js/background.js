a754932764336a70a53e95ebe50a93f2e6e8f644
sutianxin <763882220@qq.com>
2021-02-08 17:10:57 +0800

    Site updated: 2021-02-08 17:10:47

---
+++ js/background.js
@@ -0,0 +1,4 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
