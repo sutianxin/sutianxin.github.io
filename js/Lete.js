b597d0de014c169987ff7657caceea5184c1e4fc
sutianxin <763882220@qq.com>
2021-02-22 15:08:25 +0800

    Site updated: 2021-02-22 15:07:53

--- js/Lete.js
+++ js/Lete.js
@@ -1,13 +1,1 @@
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
-
-/* 手机客户端导航栏默认隐藏 */
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " closed";
-}
+var top_up="<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";document.getElementById("go-up").innerHTML+=top_up;for(var mobile_sidebar_menus=document.getElementById("mobile-sidebar-menus"),menus_item_child=mobile_sidebar_menus.getElementsByClassName("menus_item_child"),menus_expand=mobile_sidebar_menus.getElementsByClassName("expand"),i=0;i<menus_item_child.length;i++)menus_item_child[i].style.display="none",menus_expand[i].className+=" closed";
