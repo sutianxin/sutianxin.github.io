5b1e194d135325c22ac5190ad9c979ee3f1078c3
sutianxin <763882220@qq.com>
2021-03-03 23:06:48 +0800

    Site updated: 2021-03-03 23:06:28

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>var full_page = document.getElementsByClassName("full_page");
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>var full_page = document.getElementsByClassName("full_page");
 if (full_page.length != 0) {
   full_page[0].style.background = "transparent";
 }
